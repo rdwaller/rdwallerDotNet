c58eeca3cc6fc26382619430ca0a26080823139d
rdwaller <rdwaller@gmail.com>
2020-04-13 19:44:02 -0500

    Add copyright date

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-console.log('hello world!');
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentYear = new Date().getFullYear();
+  const copyright = document.getElementById('copyright');
+  copyright.innerHTML = `Copyright ${currentYear}, by Ryan Waller`;
+});
